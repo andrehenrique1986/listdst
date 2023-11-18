18bca666f6e89b301b691f04470d5426fca6f4a2
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2023-11-18 04:04:59 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -9,6 +9,7 @@ public class GameMinDTO {
      
     private Long id;
     private String title;
+    private Integer year;
     private String imgUrl;
     private String shortDescription;
 
@@ -18,6 +19,7 @@ public class GameMinDTO {
     public GameMinDTO(Game entity) {
         id = entity.getId();
         title = entity.getTitle();
+        year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
@@ -25,10 +27,10 @@ public class GameMinDTO {
      public GameMinDTO(GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
+        year = projection.getGameYear();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
     }
-
 
     public Long getId() {
         return id;
@@ -36,6 +38,10 @@ public class GameMinDTO {
 
     public String getTitle() {
         return title;
+    }
+
+    public Integer getYear() {
+        return year;
     }
 
     public String getImgUrl() {
@@ -46,7 +52,7 @@ public class GameMinDTO {
         return shortDescription;
     }
 
-    
-   
-    
+
+  
+
 }
